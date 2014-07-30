677d8aeba44b35cf490cd938f656ea51118c77fc
Ciaran Downey <me@ciarand.me>
2014-07-29 20:51:18 -0700

    Add client and basic handling logic

--- client.go
+++ client.go
@@ -1,9 +1,29 @@
 package bot
 
+import (
+	"regexp"
+)
+
 type client struct {
-	conf *config
+	conf     *config
+	handlers map[*regexp.Regexp]handler
 }
 
+type handler func(c *client)
+
 func NewClient(c *config) *client {
-	return &client{conf: c}
+	return &client{conf: c, handlers: make(map[*regexp.Regexp]handler)}
+}
+
+func (c *client) RegisterHandler(re *regexp.Regexp, h handler) {
+	c.handlers[re] = h
+}
+
+func (cl *client) handleMessage(msg string) {
+	for regex, fn := range cl.handlers {
+		if regex.MatchString(msg) {
+			go fn(cl)
+			return
+		}
+	}
 }
--- client_test.go
+++ client_test.go
@@ -1,7 +1,9 @@
 package bot
 
 import (
+	"regexp"
 	"testing"
+	"time"
 )
 
 func TestNewClient(t *testing.T) {
@@ -21,4 +23,36 @@ func TestNewClient(t *testing.T) {
 	assertSame(t, c.conf.RoomId, room)
 	assertSame(t, c.conf.FullName, full)
 	assertSame(t, c.conf.MentionName, mention)
+}
+
+func TestHandlers(t *testing.T) {
+	cli := acceptableClient()
+
+	done := make(chan bool, 1)
+
+	handler := func(c *client) {
+		done <- true
+	}
+
+	re := regexp.MustCompile("roboto")
+
+	cli.RegisterHandler(re, handler)
+	cli.handleMessage("roboto zen")
+
+	select {
+	case <-done:
+		// pass!
+		return
+	case <-time.After(1 * time.Millisecond): // hacky
+		t.Errorf("handler not called")
+	}
+}
+
+func acceptableClient() *client {
+	return NewClient(&config{
+		Username:    "77027_1062884",
+		RoomId:      "77027_developers@conf.hipchat.com",
+		FullName:    "roboto",
+		MentionName: "mention",
+	})
 }
